31ccb42adba931925b48a811c014f05bf4bb8332
Alan Munoz <alanjmr206@gmail.com>
2022-07-08 11:39:15 -0700

    update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,10 +6,8 @@ cd project-daak-alanPortfolio
 #pull all changes from repo
 git fetch && git reset origin/main --hard
 
-#Enter the python virtual environment and Install python dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+#Spin containers down to avoid memory issues
+docker compose -f docker-compose.prod.yml down
 
-#Restart myportfolio service
-systemctl daemon-reload
-systemctl restart myportfolio
+#Build the images and start the container using the .prod.yml file
+docker compose -f docker-compose.prod.yml up -d --build
